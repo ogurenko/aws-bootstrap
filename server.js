4e8b168432eff44b37b254d37ebdcbef9576c56f
gaga <pakson@gmail.com>
2022-02-22 08:19:04 +0100

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { hostname } = require("os");
 const http = require("http");
 
-const message = "Hello Cloud!";
+const message = "Hello World";
 const port = 8080;
 
 const server = http.createServer((req, res) => {
